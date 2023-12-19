353c0bc79f46db887ea30948aba066c422c4fd29
Priyansh Parikh <parikh.priyansh@gmail.com>
2023-12-19 20:06:39 +0530

    Updates

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello Welcome to JS")
+const express = require('express')
+const path = require('path')
+
+const app = express();
+
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/',(req,res,next)=>{
+    console.log('AT HOME!!')
+    res.sendFile('home.html', { root: path.join(__dirname, 'views/') });
+})
+app.listen(3000,()=>{
+    console.log('working')
+})
